3acb1381d2277e467bd854370a56c50e18c6bf22
MiguelMartinho <miguelpm2001@gmail.com>
2020-12-04 16:29:39 +0000

    Rework UI elements intancing

--- EXODataHandler.Unity/Assets/Scripts/UIDataInstantiator.cs
+++ EXODataHandler.Unity/Assets/Scripts/UIDataInstantiator.cs
@@ -6,13 +6,17 @@ using TMPro;
 // This class is responsible for placing all the rows and columns of info
 public class UIDataInstantiator : MonoBehaviour
 {
-    private int totalRows;
+    private byte totalRows;
     // maxChildren will have to be set according to the amount of paramaters given by the file
-    private byte maxChildren;
-    private float random;
+    private ushort maxChildren;
+
+    private RectTransform headerTransform;
+    private Vector3 scalechange;
+
+    private float headerWidth;
 
     [SerializeField]
-    private GameObject rowPrefab, textPrefab;
+    private GameObject rowPrefab, textPrefab, headerPrefab, headerspot;
 
     private  TextMeshProUGUI textComponent;
 
@@ -20,30 +24,51 @@ public class UIDataInstantiator : MonoBehaviour
     //Column.width = longest object in column. (THIS INFORMATION MUST COME FROM DATACORE
     private void Start()
     {
-        maxChildren = 8;
-        totalRows = 20;
-        LoadData();
-    }
-    public void LoadData() //should receive max rows and columns
+        maxChildren = 4000;
+        totalRows = 8;
+        SpawnHeaders(maxChildren);
+    } 
+    private void SpawnHeaders(int totalRows)
     {
         for (int i = 0; i < totalRows; i++)
         {
-            //generate row
-            GameObject row = Instantiate(rowPrefab, new Vector3(gameObject.transform.position.x,
-               gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
-            row.transform.SetParent(gameObject.transform);
 
-            do
-            {
-                //generate each text object
-                GameObject text = Instantiate(textPrefab);
-                text.transform.SetParent(row.transform);
-                //write in each text
-                random = Random.Range(0.0f, 100000000.0f);
-                textComponent = text.GetComponent<TextMeshProUGUI>();
-                textComponent.text = $"{random}"; //Data goes here
-            } while (row.transform.childCount != maxChildren);
+            headerTransform = headerPrefab.GetComponent<RectTransform>();
+            headerWidth = headerTransform.sizeDelta.x + 15;
 
+            GameObject header = Instantiate(headerPrefab, 
+                new Vector3(headerspot.transform.position.x + headerWidth * i,
+                headerspot.transform.position.y, headerspot.transform.position.z), Quaternion.identity);
+            header.transform.SetParent(headerspot.transform);
+            //THIS CANT BE HERE FIND A FIX vvvvvv
+            scalechange = new Vector3(2, 2, 0);
+            header.transform.localScale -= scalechange;
+            //----------------------------------
+
+            GameObject child = header.transform.GetChild(0).gameObject;
+            textComponent = child.GetComponent<TextMeshProUGUI>();
+            textComponent.text = "Nice"; //Name of parameter here
+
+        }
+        for (int i = 0; i < totalRows; i++)
+        {
+            MakeDataAppearOnHeader(headerspot);
+        }
+    }
+    private void MakeDataAppearOnHeader(GameObject headerspot)
+    {
+        for (int i = 0; i < maxChildren; i++)
+        {
+            GameObject headerToFill = headerspot.transform.GetChild(i).gameObject; //this gets the header from header spawner
+            GameObject DataZone = headerToFill.transform.GetChild(4).gameObject;
+            GameObject textchild = Instantiate(textPrefab, new Vector3(DataZone.transform.position.x, //this child is 
+                DataZone.transform.position.y, DataZone.transform.position.z), Quaternion.identity); //meant to be instantiated inside the data zone of headerTofill
+            textchild.transform.SetParent(DataZone.transform);
+
+            //write in each text
+            textComponent = textchild.GetComponent<TextMeshProUGUI>();
+            textComponent.text = $"{i}"; //Data goes here
         }
     }
 }
+
